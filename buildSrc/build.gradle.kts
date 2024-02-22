44f448cc3303a432d18a75ddf5637bed209d8bd3
Manoel Aranda Neto <marandaneto@gmail.com>
2024-02-22 12:42:12 +0100

    bump kotlin tooling

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,9 +14,9 @@ kotlin {
 
 dependencies {
     // also update PosthogBuildConfig.Kotlin.KOTLIN
-    val kotlinVersion = "1.8.22"
-    // there's no 1.8.22 for dokka yet
-    val dokkaVersion = "1.8.20"
+    val kotlinVersion = "1.9.22"
+    // there's no 1.9.22 for dokka yet
+    val dokkaVersion = "1.9.10"
     implementation("com.android.tools.build:gradle:8.1.3")
     // kotlin version has to match kotlinCompilerExtensionVersion
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
--- posthog/src/main/java/com/posthog/internal/PostHogDecideResponse.kt
+++ posthog/src/main/java/com/posthog/internal/PostHogDecideResponse.kt
@@ -1,11 +1,14 @@
 package com.posthog.internal
 
+import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement
+
 /**
  * The decide response data structure for calling the decide API
  * @property errorsWhileComputingFlags if there were errors computing feature flags
  * @property featureFlags the feature flags
  * @property featureFlagPayloads the feature flag payloads
  */
+@IgnoreJRERequirement
 internal data class PostHogDecideResponse(
     // assuming theres no errors if not present
     val errorsWhileComputingFlags: Boolean = false,
