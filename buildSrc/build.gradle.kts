64ad6e1a89f1e2773ee6c139b5ae25e28694a0eb
Manoel Aranda Neto <marandaneto@gmail.com>
2024-04-10 16:14:33 +0200

    fix

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,8 +18,8 @@ dependencies {
     val kotlinVersion = "1.8.22"
     // there's no 1.8.22 for dokka yet
     val dokkaVersion = "1.8.20"
-    // 8.3.1+ throws Could not determine the dependencies of task ':posthog:generateJvmTestLintModel'.
-    implementation("com.android.tools.build:gradle:8.3.0")
+    // 8.3+ throws Could not determine the dependencies of task ':posthog:generateJvmTestLintModel'.
+    implementation("com.android.tools.build:gradle:8.2.2")
     // kotlin version has to match kotlinCompilerExtensionVersion
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
