492b8abea2c7194529b20a86e729022ba8d16a6f
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-11 16:48:13 +0200

    fix

--- posthog-samples/posthog-android-sample/src/main/java/com/posthog/android/sample/MyApp.kt
+++ posthog-samples/posthog-android-sample/src/main/java/com/posthog/android/sample/MyApp.kt
@@ -2,7 +2,6 @@ package com.posthog.android.sample
 
 import android.app.Application
 import android.os.StrictMode
-import com.posthog.PostHog
 import com.posthog.PostHogOnFeatureFlags
 import com.posthog.PostHogPropertiesSanitizer
 import com.posthog.android.PostHogAndroid
@@ -43,7 +42,6 @@ class MyApp : Application() {
                 sessionReplayConfig.screenshot = true
             }
         PostHogAndroid.setup(this, config)
-        PostHog.capture("test")
     }
 
     private fun enableStrictMode() {
