38fbb0c74f10d9db8c5d7e696433b97e888ce2e0
dylan <dylan@posthog.com>
2025-02-25 14:52:00 -0800

    formatting

--- posthog/src/main/java/com/posthog/internal/PostHogFeatureFlags.kt
+++ posthog/src/main/java/com/posthog/internal/PostHogFeatureFlags.kt
@@ -90,18 +90,17 @@ internal class PostHogFeatureFlags(
                 response?.let {
                     synchronized(featureFlagsLock) {
                         if (response.quotaLimited?.contains("feature_flags") == true) {
-                            config.logger.log("Feature flags are quota limited, clearing existing flags. Learn more about billing limits at https://posthog.com/docs/billing/limits-alerts")
+                            config.logger.log(
+                                "Feature flags are quota limited, clearing existing flags. " +
+                                    "Learn more about billing limits at https://posthog.com/docs/billing/limits-alerts",
+                            )
                             this.featureFlags = null
                             this.featureFlagPayloads = null
                             config.cachePreferences?.let { preferences ->
                                 preferences.remove(FEATURE_FLAGS)
                                 preferences.remove(FEATURE_FLAGS_PAYLOAD)
                             }
-<<<<<<< HEAD
-                            return@executeSafely
-=======
                             return@let
->>>>>>> main
                         }
 
                         if (response.errorsWhileComputingFlags) {
