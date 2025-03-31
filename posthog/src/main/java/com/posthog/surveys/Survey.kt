01584a4907560016726344920a0a5991f01a950e
Manoel Aranda Neto <marandaneto@gmail.com>
2025-03-31 13:56:55 +0200

    serir issue

--- posthog/src/main/java/com/posthog/surveys/Survey.kt
+++ posthog/src/main/java/com/posthog/surveys/Survey.kt
@@ -1,3 +1,4 @@
+import com.google.gson.annotations.SerializedName
 import com.posthog.surveys.SurveyAppearance
 import com.posthog.surveys.SurveyConditions
 import com.posthog.surveys.SurveyFeatureFlagKeyValue
@@ -11,14 +12,22 @@ public data class Survey(
     val type: SurveyType,
     val questions: List<SurveyQuestion>,
     val description: String?,
+    @SerializedName("feature_flag_keys")
     val featureFlagKeys: List<SurveyFeatureFlagKeyValue>?,
+    @SerializedName("linked_flag_key")
     val linkedFlagKey: String?,
+    @SerializedName("targeting_flag_key")
     val targetingFlagKey: String?,
+    @SerializedName("internal_targeting_flag_key")
     val internalTargetingFlagKey: String?,
     val conditions: SurveyConditions?,
     val appearance: SurveyAppearance?,
+    @SerializedName("current_iteration")
     val currentIteration: Int?,
+    @SerializedName("current_iteration_start_date")
     val currentIterationStartDate: Date?,
+    @SerializedName("start_date")
     val startDate: Date?,
+    @SerializedName("end_date")
     val endDate: Date?,
 )
