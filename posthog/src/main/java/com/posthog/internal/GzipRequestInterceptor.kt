5a34dca39af50422f5468f6c2e6873ca47b9eb1c
Manoel Aranda Neto <marandaneto@gmail.com>
2025-06-13 15:33:22 +0200

    fix

--- posthog/src/main/java/com/posthog/internal/GzipRequestInterceptor.kt
+++ posthog/src/main/java/com/posthog/internal/GzipRequestInterceptor.kt
@@ -20,8 +20,14 @@ package com.posthog.internal
 
 import com.posthog.PostHogConfig
 import okhttp3.Interceptor
+import okhttp3.MediaType
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.Response
+import okio.Buffer
+import okio.BufferedSink
+import okio.GzipSink
+import okio.buffer
 import java.io.IOException
 
 // https://square.github.io/okhttp/features/interceptors/
@@ -46,6 +52,7 @@ internal class GzipRequestInterceptor(private val config: PostHogConfig) : Inter
                 try {
                     originalRequest.newBuilder()
                         .header("Content-Encoding", "gzip")
+//                        .method(originalRequest.method, forceContentLength(gzip(body)))
                         .build()
                 } catch (e: Throwable) {
                     config.logger.log("Failed to gzip the request body: $e.")
@@ -53,6 +60,47 @@ internal class GzipRequestInterceptor(private val config: PostHogConfig) : Inter
                     originalRequest
                 }
             chain.proceed(compressedRequest)
+        }
+    }
+
+    private fun gzip(body: RequestBody): RequestBody {
+        return object : RequestBody() {
+            override fun contentType(): MediaType? {
+                return body.contentType()
+            }
+
+            override fun contentLength(): Long {
+                return -1 // We don't know the compressed length in advance!
+            }
+
+            @Throws(IOException::class)
+            override fun writeTo(sink: BufferedSink) {
+                val gzipSink = GzipSink(sink).buffer()
+                body.writeTo(gzipSink)
+                gzipSink.close()
+            }
+        }
+    }
+
+    // https://github.com/square/okhttp/issues/350
+    @Throws(IOException::class)
+    private fun forceContentLength(body: RequestBody): RequestBody {
+        val buffer = Buffer()
+        body.writeTo(buffer)
+
+        return object : RequestBody() {
+            override fun contentType(): MediaType? {
+                return body.contentType()
+            }
+
+            override fun contentLength(): Long {
+                return buffer.size
+            }
+
+            @Throws(IOException::class)
+            override fun writeTo(sink: BufferedSink) {
+                sink.write(buffer.snapshot())
+            }
         }
     }
 }
