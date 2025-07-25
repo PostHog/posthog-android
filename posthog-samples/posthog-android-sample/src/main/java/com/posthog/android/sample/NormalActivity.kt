6b3bb41af1fea97fb98d4f816996a45af0d3e096
KopeikinaDarya <darya@yomobile.com>
2025-07-25 15:28:08 +0300

    add example

--- posthog-samples/posthog-android-sample/src/main/java/com/posthog/android/sample/NormalActivity.kt
+++ posthog-samples/posthog-android-sample/src/main/java/com/posthog/android/sample/NormalActivity.kt
@@ -1,69 +1,41 @@
 package com.posthog.android.sample
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.util.Log
 import androidx.activity.ComponentActivity
-import com.posthog.PostHog
-import com.posthog.PostHogOkHttpInterceptor
-import okhttp3.OkHttpClient
-import okhttp3.internal.closeQuietly
+import com.posthog.PersonProfiles.ALWAYS
+import com.posthog.PostHogInterface
+import com.posthog.PostHogOnFeatureFlags
+import com.posthog.android.PostHogAndroid
+import com.posthog.android.PostHogAndroidConfig
+import com.posthog.android.replay.PostHogSessionReplayConfig
 
 class NormalActivity : ComponentActivity() {
-    private val client =
-        OkHttpClient.Builder()
-            .addInterceptor(PostHogOkHttpInterceptor(captureNetworkTelemetry = true))
-            .build()
+
+    private var postHog: PostHogInterface? = null
+
+    private fun getConfig() = PostHogAndroidConfig(
+        apiKey = "KEY", //todo set KEY
+        host = "HOST",  //todo set HOST
+        captureApplicationLifecycleEvents = true,
+        captureDeepLinks = false,
+        captureScreenViews = false,
+        sessionReplayConfig = PostHogSessionReplayConfig()
+    ).apply {
+        sendFeatureFlagEvent = true
+        preloadFeatureFlags = true
+        debug = BuildConfig.DEBUG
+        personProfiles = ALWAYS
+        onFeatureFlags = PostHogOnFeatureFlags {
+            postHog?.let {
+                Log.d("PosthogTest", "onFeatureFlags = $it")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.normal_activity)
-
-//        val webview = findViewById<WebView>(R.id.webview)
-//        webview.loadUrl("https://www.google.com")
-
-        val button = findViewById<Button>(R.id.button)
-//        val editText = findViewById<EditText>(R.id.editText)
-//        val imvAndroid = findViewById<ImageView>(R.id.imvAndroid)
-        button.setOnClickListener {
-//            Log.e("MyApp", "Clicked on button ${button.text}")
-//            button.text = "Test: ${(0..10).random()}"
-//            if (imvAndroid.visibility == View.VISIBLE) {
-//                imvAndroid.visibility = View.GONE
-//            } else {
-//                imvAndroid.visibility = View.VISIBLE
-//            }
-//            startActivity(Intent(this, NothingActivity::class.java))
-//            finish()
-            // Check if the "enable_network_request" feature flag is enabled
-            val isNetworkRequestEnabled = PostHog.isFeatureEnabled("enable_network_request", false)
-
-            if (isNetworkRequestEnabled) {
-                // Make the network request
-                Thread {
-                    try {
-                        client.newCall(
-                            okhttp3.Request.Builder()
-                                .url("https://google.com")
-                                .build(),
-                        ).execute().closeQuietly()
-
-                        // Show success message on the main thread
-                        runOnUiThread {
-                            Toast.makeText(this, "Network request successful!", Toast.LENGTH_SHORT).show()
-                        }
-                    } catch (e: Exception) {
-                        // Show error message on the main thread
-                        runOnUiThread {
-                            Toast.makeText(this, "Network request failed: ${e.message}", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }.start()
-            } else {
-                // Show message that feature is disabled
-                Toast.makeText(this, "Network requests are disabled by feature flag: `enable_network_request`", Toast.LENGTH_SHORT).show()
-            }
-        }
+        postHog = PostHogAndroid.with(this, getConfig())
     }
 }
