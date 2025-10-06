41d55a4b0fa7bfc7683e5cbc699629d4ccf10291
Dustin Byrne <dustin@posthog.com>
2025-10-06 18:07:01 -0400

    chore: Update Java sample to include local eval

--- posthog-samples/posthog-java-sample/src/main/java/com/posthog/java/sample/PostHogJavaExample.java
+++ posthog-samples/posthog-java-sample/src/main/java/com/posthog/java/sample/PostHogJavaExample.java
@@ -1,23 +1,55 @@
 package com.posthog.java.sample;
 
+import com.posthog.server.PostHog;
 import com.posthog.server.PostHogCaptureOptions;
 import com.posthog.server.PostHogConfig;
-import com.posthog.server.PostHog;
+import com.posthog.server.PostHogFeatureFlagOptions;
 import com.posthog.server.PostHogInterface;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Simple Java 1.8 example demonstrating PostHog usage
  */
 public class PostHogJavaExample {
+    private static PostHogInterface postHog;
+
     public static void main(String[] args) {
         PostHogConfig config = PostHogConfig
-                .builder("phc_wz4KZkikEluCCdfY2B2h7MXYygNGdTqFgjbU7I1ZdVR")
+                .builder("phc_qYXiHw5odMiVWF7Dwh2sHWS7Hj6FsutBNp2SEaMqS0A")
+                .personalApiKey("phx_example")
+                .host("http://localhost:8010")
+                .localEvaluation(true)
+                .debug(true)
+                .onFeatureFlags(() -> {
+                    if (postHog.isFeatureEnabled("distinct-id", "beta-feature", false)) {
+                        System.out.println("The feature is enabled.");
+                    }
+
+                    Object flagValue = postHog.getFeatureFlag("distinct-id", "multi-variate-flag", "default");
+                    String flagVariate = flagValue instanceof String ? (String) flagValue : "default";
+                    Object flagPayload = postHog.getFeatureFlagPayload("distinct-id", "multi-variate-flag");
+
+                    System.out.println("The flag variant was: " + flagVariate);
+                    System.out.println("Received flag payload: " + flagPayload);
+
+                    Boolean hasFilePreview = postHog.isFeatureEnabled(
+                            "distinct-id",
+                            "file-previews",
+                            PostHogFeatureFlagOptions
+                                    .builder()
+                                    .defaultValue(false)
+                                    .personProperty("email", "example@example.com")
+                                    .build());
+
+                    System.out.println("File previews enabled: " + hasFilePreview);
+
+                    postHog.flush();
+                    postHog.close();
+                })
                 .build();
 
-        PostHogInterface postHog = PostHog.with(config);
+        postHog = PostHog.with(config);
 
         postHog.group("distinct-id", "company", "some-company-id");
         postHog.capture(
@@ -36,24 +68,9 @@ public class PostHogJavaExample {
         // AVOID - Anonymous inner class holds reference to outer class.
         // The following won't serialize properly.
         // postHog.identify("user-123", new HashMap<String, Object>() {{
-        //      put("key", "value");
+        // put("key", "value");
         // }});
 
         postHog.alias("distinct-id", "alias-id");
-
-
-        if (postHog.isFeatureEnabled("distinct-id", "beta-feature", false)) {
-            System.out.println("The feature is enabled.");
-        }
-
-        Object flagValue = postHog.getFeatureFlag("distinct-id", "multi-variate-flag", "default");
-        String flagVariate = flagValue instanceof String ? (String) flagValue : "default";
-        Object flagPayload = postHog.getFeatureFlagPayload("distinct-id", "multi-variate-flag");
-
-        System.out.println("The flag variant was: " + flagVariate);
-        System.out.println("Received flag payload: " + flagPayload);
-
-        postHog.flush();
-        postHog.close();
     }
 }
