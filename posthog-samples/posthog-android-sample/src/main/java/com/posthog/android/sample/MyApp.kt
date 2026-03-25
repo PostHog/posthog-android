b3b3d0b88d97c0846d2419bbd06f69cf4cb9f65e
Ioannis J <yiannis@posthog.com>
2026-03-25 09:32:16 +0200

    fix: revert sample app changes

--- posthog-samples/posthog-android-sample/src/main/java/com/posthog/android/sample/MyApp.kt
+++ posthog-samples/posthog-android-sample/src/main/java/com/posthog/android/sample/MyApp.kt
@@ -15,7 +15,7 @@ class MyApp : Application() {
         // Demo:
 //        val apiKey = "_6SG-F7I1vCuZ-HdJL3VZQqjBlaSb1_20hDPwqMNnGI"
         // ManoelTesting:
-        val apiKey = "phc_WKfvDfedaJEDCoUmt9pVa3OWtbbUP1W2ctxwXkt3A3n"
+        val apiKey = "phc_6lqCaCDCBEWdIGieihq5R2dZpPVbAUFISA75vFZow06"
         // PaulKey
 //        val apiKey = "phc_GavhjwMwc75N4HsaLjMTEvH8Kpsz70rZ3N0E9ho89YJ"
 //        val config = PostHogAndroidConfig(apiKey, host = "https://3727-86-27-112-156.ngrok-free.app").apply {
@@ -41,7 +41,7 @@ class MyApp : Application() {
                 sessionReplayConfig.maskAllImages = false
                 sessionReplayConfig.captureLogcat = true
                 sessionReplayConfig.screenshot = true
-                surveys = false
+                surveys = true
                 errorTrackingConfig.autoCapture = true
             }
         PostHogAndroid.setup(this, config)
