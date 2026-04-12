1722da44c69e5f8e7984535927cf073f1d953957
Brett Willis <brettjohnwillis@gmail.com>
2026-04-13 09:46:53 +1200

    Add executable and env inputs to CLI task
    Make tasks public so inputs/outputs can be configured in the end project

--- posthog-android-gradle-plugin/src/main/kotlin/com/posthog/android/PostHogCliExecTask.kt
+++ posthog-android-gradle-plugin/src/main/kotlin/com/posthog/android/PostHogCliExecTask.kt
@@ -3,20 +3,57 @@
 package com.posthog.android
 
 import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.work.DisableCachingByDefault
 
 @DisableCachingByDefault(because = "abstract task, should not be used directly")
-internal abstract class PostHogCliExecTask : Exec() {
+public abstract class PostHogCliExecTask : Exec() {
+
+    @get:Input
+    public abstract val postHogExecutable: Property<String>
+
+    @get:Input
+    @get:Optional
+    public abstract val postHogHost: Property<String>
+
+    @get:Input
+    @get:Optional
+    public abstract val postHogProjectId: Property<String>
+
+    @get:Input
+    @get:Optional
+    public abstract val postHogApiKey: Property<String>
+
+    init {
+        postHogExecutable.convention("posthog-cli")
+    }
+
     override fun exec() {
-        computeCommandLineArgs().let {
-            commandLine(it)
+        executable = postHogExecutable.get()
+
+        val args = computeCommandLineArgs().also {
             logger.info("cli args: $it")
         }
+        args(args)
+
+        // Setup environment variables for authentication etc
+        postHogHost.orNull?.let {
+            environment("POSTHOG_CLI_HOST", it)
+        }
+        postHogProjectId.orNull?.let {
+            environment("POSTHOG_CLI_PROJECT_ID", it)
+        }
+        postHogApiKey.orNull?.let {
+            environment("POSTHOG_CLI_API_KEY", it)
+        }
+
         super.exec()
     }
 
-    abstract fun getArguments(args: MutableList<String>)
+    protected abstract fun getArguments(args: MutableList<String>)
 
     /** Computes the full list of arguments for the task */
     private fun computeCommandLineArgs(): List<String> {
@@ -25,9 +62,6 @@ internal abstract class PostHogCliExecTask : Exec() {
             args.add(0, "cmd")
             args.add(1, "/c")
         }
-
-        // TODO: CLI path config
-        args.add("posthog-cli")
 
         getArguments(args)
 
